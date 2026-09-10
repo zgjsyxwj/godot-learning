dc6a10717ea3287d7de443b9d5cdae3119f25e0a
Seed.Wu <seed.wu@cdpgroupltd.com>
2026-09-10 16:59:29 +0800

    feat: 添加八方向角色移动 Demo 和学习记录

---
+++ godot-learning/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+@export var speed: float = 300.0
+
+func _physics_process(_delta: float) -> void:
+	var direction := Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_up",
+        "move_down"
+	)
+	velocity = direction * speed
+	move_and_slide()
