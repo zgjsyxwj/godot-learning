480ea20a8fafc5eee61d2afeabda5562a11224cf
Seed.Wu <seed.wu@cdpgroupltd.com>
2026-09-18 16:58:59 +0800

    feat(godot): 切换至按钮与定时器信号练习场景

--- godot-learning/sprite_2d.gd
+++ godot-learning/sprite_2d.gd
@@ -4,15 +4,20 @@ var speed = 400
 var angular_speed = PI
 
 
-func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
-		position += velocity * delta
+func _ready() -> void:
+	var timer = get_node("Timer")
+	timer.timeout.connect(_on_timer_timeout)
 
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
-	rotation += angular_speed * direction * delta
+
+func _process(delta: float) -> void:
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
+
+
+func _on_button_pressed() -> void:
+	set_process(not is_processing())
+
+
+func _on_timer_timeout():
+	visible = not visible
